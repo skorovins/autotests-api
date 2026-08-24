7e4a67f0d7fb2a8e16a5a8d39ab3395f8ab22cc5
skorovins <skorovins@yandex.ru>
2026-08-24 16:50:26 +0300

    Обновления: - добавлен event_hooks.py для прикрепления к Allure отчету cURL команду - добавлен curl.py для генерации cURL команды - добавление event_hook в билдеры private_http_builder.py и public_http_builder.py

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, ConfigDict
 from httpx import Client
 from clients.authentication.authentication_client import get_authentication_client
 from clients.authentication.authentication_schema import LoginRequestSchema
+from clients.event_hooks import curl_event_hook
 
 
 class AuthenticationUserSchema(BaseModel):
@@ -33,5 +34,6 @@ def get_private_http_client(user: AuthenticationUserSchema) -> Client:
         timeout=100,
         base_url="http://localhost:8000",
         # Значения теперь извлекаем не по ключу, а через атрибуты
-        headers={"Authorization": f"Bearer {login_response.token.access_token}"}
+        headers={"Authorization": f"Bearer {login_response.token.access_token}"},
+        event_hooks={"request": [curl_event_hook]}
     )
